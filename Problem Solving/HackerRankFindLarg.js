46ad4c90f3501a48f2d9519a973fb6e90f4077f2
jakir540 <jakirhossainmd540@gamil.com>
2023-03-26 11:09:06 +0600

    Problem Solving

---
+++ Problem Solving/HackerRankFindLarg.js
@@ -0,0 +1,19 @@
+
+var twoSum = function(nums, target) {
+    console.log(nums[nums.length-1])
+   let i = 0;
+   let j = nums.length-1;
+   let sum = 0
+   while(i < j){
+    sum = nums[i] + nums[j];
+
+    sum < target ? j--:i++ 
+    
+   }
+
+return sum
+};
+
+let nums = [2,7,11,15]
+let result  =twoSum(nums,9);
+console.log(result)
